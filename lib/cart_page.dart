022f0e74f474fcf5b1010d05384056f65437ea9b
malaka96 <madhubhashana655@gmail.com>
2025-05-31 12:20:14 +0530

    state management

---
+++ lib/cart_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/cart_provider.dart';
+import 'package:shop_app/global_variable.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print(Provider.of<CartProvider>(context).cart);
+    return Scaffold(
+      appBar: AppBar(title: Text('Cart'), centerTitle: true),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          final cartItem = cart[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+              radius: 30,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete),
+              color: Colors.red,
+            ),
+            title: Text(
+              cartItem['title'].toString(),
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            subtitle: Text('Size: ${cartItem['size']}'),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/global_variable.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/cart_provider.dart';
+//import 'package:shop_app/global_variable.dart';
 import 'package:shop_app/home_page.dart';
-import 'package:shop_app/product_details_page.dart';
+//import 'package:shop_app/product_details_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,29 +14,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shoppng App',
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        appBarTheme: AppBarTheme(
-          titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
+    return ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MaterialApp(
+        title: 'Shoppng App',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          appBarTheme: AppBarTheme(
+            titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+          ),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Color.fromRGBO(254, 206, 1, 1),
+            primary: Color.fromRGBO(254, 206, 1, 1),
+          ),
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+            titleMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            bodySmall: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          useMaterial3: true,
         ),
-        inputDecorationTheme: InputDecorationTheme(
-          hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
-        ),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromRGBO(254, 206, 1, 1),
-          primary: Color.fromRGBO(254, 206, 1, 1),
-        ),
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
-          titleMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          bodySmall: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-        ),
-        useMaterial3: true,
+        home: HomePage(),
       ),
-      home: ProductDetailsPage(product: products[0]),
     );
   }
 }
